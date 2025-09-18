e4e919dec81487268885ea071fe02bb63e2658a3
Diao Diallo <ddiao7414@gmail.com>
2025-09-18 21:54:21 +0000

    Ajout du site DIAO SERVICES avec toutes les pages et animations

---
+++ script.js
@@ -0,0 +1,30 @@
+const scrollElements = document.querySelectorAll('.scroll-animation');
+
+const elementInView = (el, offset = 100) => {
+  const elementTop = el.getBoundingClientRect().top;
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) - offset
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add('visible');
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove('visible');
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 100)) {
+      displayScrollElement(el);
+    } else {
+      hideScrollElement(el);
+    }
+  });
+};
+
+window.addEventListener('scroll', () => { handleScrollAnimation(); });
+handleScrollAnimation();
